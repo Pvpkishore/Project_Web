18194fa618e9aafa09a3b3309e35c26cc69b8ff0
pvpkishore <pvpkishore09@gmail.com>
2025-01-12 14:58:13 +0530

    Added new feature

--- src/Data/projects.jsx
+++ src/Data/projects.jsx
@@ -1,23 +1,30 @@
-import img1 from "../Assets/jsprojectscreenshots/Screenshot(64).png";
-import img2 from "../Assets/jsprojectscreenshots/Screenshot(69).png";
-import img3 from "../Assets/jsprojectscreenshots/Screenshot(96).png";
-import img4 from "../Assets/jsprojectscreenshots/Screenshot(70).png";
-import img5 from "../Assets/jsprojectscreenshots/Screenshot(71).png";
-import img6 from "../Assets/jsprojectscreenshots/Screenshot(66).png";
-import img7 from "../Assets/jsprojectscreenshots/Screenshot(65).png";
-import img8 from "../Assets/jsprojectscreenshots/Screenshot(88).png";
-import img10 from "../Assets/ReactScreenshots/Screenshot(55).png";
-import img11 from "../Assets/ReactScreenshots/Screenshot(92).png";
-import img12 from "../Assets/ReactScreenshots/Screenshot(86).png";
-import img13 from "../Assets/ReactScreenshots/Screenshot(84).png";
-import img14 from "../Assets/ReactScreenshots/Screenshot(80).png";
-import img15 from "../Assets/ReactScreenshots/Screenshot(83).png";
-import img16 from "../Assets/Backendscreenshots/Screenshot(90).png";
-import img17 from "../Assets/Backendscreenshots/Screenshot(89).png";
-import img18 from "../Assets/Backendscreenshots/Screenshotssecrets.png";
-import img19 from "../Assets/Backendscreenshots/Screenshotspassop.png";
-import img20 from "../Assets/Backendscreenshots/Screenshot(95).png";
-
+import img1 from "../Assets/jsprojectscreenshots/Screenshot(62).png";
+import img2 from "../Assets/jsprojectscreenshots/Screenshot(64).png";
+import img3 from "../Assets/jsprojectscreenshots/Screenshot(69).png";
+import img4 from "../Assets/jsprojectscreenshots/Screenshot(96).png";
+import img5 from "../Assets/jsprojectscreenshots/Screenshot(70).png";
+import img6 from "../Assets/jsprojectscreenshots/Screenshot(71).png";
+import img7 from "../Assets/jsprojectscreenshots/Screenshot(66).png";
+import img8 from "../Assets/jsprojectscreenshots/Screenshot(65).png";
+import img9 from "../Assets/jsprojectscreenshots/Screenshot(88).png";
+import img10 from "../Assets/jsprojectscreenshots/Tictactoe.png";
+import img11 from "../Assets/jsprojectscreenshots/stopwatch.png";
+import img12 from "../Assets/jsprojectscreenshots/microsoft.png.png";
+import img13 from "../Assets/jsprojectscreenshots/resland.png";
+import img14 from "../Assets/ReactScreenshots/Screenshot(55).png";
+import img15 from "../Assets/ReactScreenshots/Screenshot(92).png";
+import img16 from "../Assets/ReactScreenshots/Screenshot(86).png";
+import img17 from "../Assets/ReactScreenshots/Screenshot(84).png";
+import img18 from "../Assets/ReactScreenshots/Screenshot(80).png";
+import img19 from "../Assets/ReactScreenshots/Screenshot(83).png";
+import img20 from "../Assets/ReactScreenshots/calcii.png";
+import img21 from "../Assets/Backendscreenshots/Screenshot(90).png";
+import img22 from "../Assets/Backendscreenshots/Screenshot(89).png";
+import img23 from "../Assets/Backendscreenshots/Screenshotssecrets.png";
+import img24 from "../Assets/Backendscreenshots/Screenshotspassop.png";
+import img25 from "../Assets/Backendscreenshots/Screenshot(95).png";
+import img26 from "../Assets/Backendscreenshots/linktree.png";
+import img27 from "../Assets/Backendscreenshots/Bookstoreimg.png";
 
 export const projects = {
   javascript: [
@@ -33,7 +40,7 @@ export const projects = {
     },
     {
       id: 2,
-      imgSrc: img1,
+      imgSrc: img2,
       alt: "News Flash Screenshot",
       title: "News Flash",
       description:
@@ -43,7 +50,7 @@ export const projects = {
     },
     {
       id: 3,
-      imgSrc: img2,
+      imgSrc: img3,
       alt: "MusicX Screenshot",
       title: "MusicX",
       description:
@@ -53,7 +60,7 @@ export const projects = {
     },
     {
       id: 4,
-      imgSrc: img3,
+      imgSrc: img4,
       alt: "Professional Todo-List Screenshot",
       title: "Professional Todo-List",
       description:
@@ -63,7 +70,7 @@ export const projects = {
     },
     {
       id: 5,
-      imgSrc: img4,
+      imgSrc: img5,
       alt: "Portfolio v1.0.0 Screenshot",
       title: "Portfolio v1.0.0",
       description:
@@ -73,7 +80,7 @@ export const projects = {
     },
     {
       id: 6,
-      imgSrc: img5,
+      imgSrc: img6,
       alt: "WeatherX Screenshot",
       title: "WeatherX",
       description:
@@ -83,7 +90,7 @@ export const projects = {
     },
     {
       id: 7,
-      imgSrc: img6,
+      imgSrc: img7,
       alt: "Drum Kit Screenshot",
       title: "Drum Kit",
       description:
@@ -93,7 +100,7 @@ export const projects = {
     },
     {
       id: 8,
-      imgSrc: img7,
+      imgSrc: img8,
       alt: "Dice Game Screenshot",
       title: "Dice Game",
       description:
@@ -103,7 +110,7 @@ export const projects = {
     },
     {
       id: 9,
-      imgSrc: img8,
+      imgSrc: img9,
       alt: "Load More Button API Call Screenshot",
       title: "Load More Button API Call",
       description:
@@ -111,11 +118,50 @@ export const projects = {
       liveLink: "https://github.com/Pvpkishore/Load_More_Button_API_call",
       githubLink: "https://github.com/Pvpkishore/Load_More_Button_API_call",
     },
+    {
+      id: 9,
+      imgSrc: img10,
+      alt: "Tic Tac Toe Game",
+      title: "Tic Tac Toe Game",
+      description:'This project is a classic Tic Tac Toe game built using HTML, CSS, and JavaScript, with an added feature of an AI opponent to challenge players. The game follows the standard 3x3 grid, where two players take turns marking their chosen cells with X and O. The game checks for winning conditions (three in a row horizontally, vertically, or diagonally) and declares a winner or a draw.',
+      liveLink: "https://aitic-tac-toewebapplicationkishore.vercel.app/",
+      githubLink: "https://github.com/Pvpkishore/Prodigy_InfoTech_Internship/tree/main/PRODIGY_WD_03(Tic-Tac-Toe%20Web%20application)",
+    },
+    {
+      id: 9,
+      imgSrc: img11,
+      alt: "StopWatch",
+      title: "StopWatch",
+      description:
+        'This project is a Stopwatch built using HTML, CSS, and JavaScript, enhanced with the ability to record lap times. The stopwatch allows users to start, stop, reset the timer, and record lap times, displaying the elapsed time for each lap. The interface remains minimalistic and user-friendly, showing the time in minutes, seconds and individual lap times.',
+      liveLink: "https://stopwatchwebapplicationkishore.vercel.app/",
+      githubLink: "https://github.com/Pvpkishore/Prodigy_InfoTech_Internship/tree/main/PRODIGY_WD_02(StopWatch%20Web%20Application)",
+    },
+    {
+      id: 9,
+      imgSrc: img12,
+      alt: "Microsoft-page",
+      title: "Microsoft-page",
+      description:
+        'This project is a Microsoft Landing Page designed with Tailwind CSS, providing a clean, modern, and professional layout to showcase Microsoft products and services. The design is inspired by the simplicity and user-centric approach often seen in Microsoft web presence. It includes a responsive header, main content area, features section, and footer.',
+      liveLink: "https://microsoft-page-kishore.vercel.app/",
+      githubLink: "https://github.com/Pvpkishore/Microsoft-page",
+    },
+    {
+      id: 9,
+      imgSrc: img13,
+      alt: "Load More Button API Call Screenshot",
+      title: "Prodigy landing-page",
+      description:
+        'This project is a Prodigy Landing Page built using HTML, CSS, and Javascript. It is designed to showcase a modern, engaging, and professional user interface for Prodigy, which could represent a product, service, or application. The layout is responsive, ensuring an optimal viewing experience across devices, from desktops to mobile phones.',
+      liveLink: "https://responsivelandingpage-kishore.vercel.app/",
+      githubLink: "https://github.com/Pvpkishore/Prodigy_InfoTech_Internship/tree/main/PRODIGY_WD_01(Landing%20page)",
+    },
   ],
 react: [
     {
       id: 10,
-      imgSrc: img10,
+      imgSrc: img14,
       alt: 'GitHub Glazer Screenshot',
       title: 'GitHub Glazer',
       description:
@@ -125,7 +171,7 @@ react: [
     },
     {
       id: 11,
-      imgSrc: img11,
+      imgSrc: img15,
       alt: 'Portfolio v2.0.0 Screenshot',
       title: 'Portfolio v2.0.0',
       description:
@@ -135,7 +181,7 @@ react: [
     },
     {
       id: 12,
-      imgSrc: img12,
+      imgSrc: img16,
       alt: 'Random Quote Generator Screenshot',
       title: 'Random Quote Generator',
       description:
@@ -145,7 +191,7 @@ react: [
     },
     {
       id: 13,
-      imgSrc: img13,
+      imgSrc: img17,
       alt: 'Simple Form Validation Screenshot',
       title: 'Simple Form Validation',
       description:
@@ -155,7 +201,7 @@ react: [
     },
     {
       id: 14,
-      imgSrc: img14,
+      imgSrc: img18,
       alt: 'Keeper App Screenshot',
       title: 'Keeper App',
       description:
@@ -165,7 +211,7 @@ react: [
     },
     {
       id: 15,
-      imgSrc: img15,
+      imgSrc: img19,
       alt: 'Todo-List Screenshot',
       title: 'Todo-List',
       description:
@@ -173,11 +219,21 @@ react: [
       liveLink: 'https://todo-list-react-kishore.vercel.app/',
       githubLink: 'https://github.com/Pvpkishore/Todo-list-React',
     },
+    {
+      id: 15,
+      imgSrc: img20,
+      alt: 'Scientific Calculator',
+      title: 'Scientific Calculator',
+      description:
+        'This project is a Scientific Calculator built with React.js for functionality and Tailwind CSS for styling. It includes standard arithmetic functions (addition, subtraction, multiplication, division) and advanced scientific functions like square root, exponentiation, trigonometric functions (sin, cos, tan), and logarithms.',
+      liveLink: 'https://scientific-calculator-kishore.vercel.app/',
+      githubLink: 'https://github.com/Pvpkishore/scientific-calculator',
+    },
   ],
   backend: [
     {
       id: 16,
-      imgSrc: img16,
+      imgSrc: img21,
       alt: 'YouTube Video Converter Project Screenshot',
       title: 'Youtube Video Converter',
       description:
@@ -187,7 +243,7 @@ react: [
     },
     {
       id: 17,
-      imgSrc: img17,
+      imgSrc: img22,
       alt: "Let's Chat Project Screenshot",
       title: "Let's Chat",
       description:
@@ -197,7 +253,7 @@ react: [
     },
     {
       id: 18,
-      imgSrc: img18,
+      imgSrc: img23,
       alt: 'Secret Dialog Project Screenshot',
       title: 'Secret Dialog',
       description:
@@ -209,7 +265,7 @@ react: [
   fullstack: [
     {
       id: 19,
-      imgSrc: img19,
+      imgSrc: img24,
       alt: 'PassOP Project Screenshot',
       title: 'PassOP',
       description:
@@ -219,11 +275,31 @@ react: [
     },
     {
       id: 20,
-      imgSrc: img20,
+      imgSrc: img25,
       alt: 'AI Talking Bot Project Screenshot',
       title: 'AI Talking Bot',
       description:
         'AI Talking Chatbot, built using React.js, Express.js, and the OpenAI API, enables users to interact with an AI through voice commands. It can respond to queries, hold conversations, and perform actions like sending or resetting messages via voice commands.',
+      liveLink: '', // Add the live link if available
+      githubLink: '', // Add the GitHub link if available
+    },
+    {
+      id: 20,
+      imgSrc: img26,
+      alt: 'Linktree Clone Project Screenshot',
+      title: 'Linktree Clone',
+      description:
+        'This project is a Linktree Clone built using Next.js for server-side rendering and dynamic routing, along with Tailwind CSS for styling. It mimics the functionality of Linktree, allowing users to create a personalized page that houses multiple links (e.g., social media profiles, websites, and more) in one place.',
+      liveLink: 'https://linktrekishore.vercel.app', // Add the live link if available
+      githubLink: 'https://github.com/Pvpkishore/my-app', // Add the GitHub link if available
+    },
+    {
+      id: 20,
+      imgSrc: img27,
+      alt: 'Bookstore Project Screenshot',
+      title: 'Bookstore',
+      description:
+        'This Bookstore app, built with the MERN stack, includes login authentication to access features like the Courses page. It displays books with details fetched from a MongoDB database. Fully responsive, it features a dark and light theme toggle, styled with Tailwind CSS for a modern user experience.',
       liveLink: '', // Add the live link if available
       githubLink: '', // Add the GitHub link if available
     },
